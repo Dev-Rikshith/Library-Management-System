a671bfd84c2cd7e9f773f5c8a4f992e6c421625b
Sohan Kotha <sohankotha@gmail.com>
2020-08-31 22:05:19 +0530

    .

--- src/com/commandline/librarymanagement/Main.java
+++ src/com/commandline/librarymanagement/Main.java
@@ -1,5 +1,7 @@
 package com.commandline.librarymanagement;
 
+import com.commandline.facultyMangement.FacultyDetails;
+import com.commandline.facultyMangement.FacultyManagement;
 import com.commandline.studentmangement.StudentDetails;
 import com.commandline.studentmangement.StudentManagement;
 
@@ -7,15 +9,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StudentManagement studentManagement = new StudentManagement();
-        studentManagement.addStudents();
-//        StudentDetails studentDetails = studentManagement.removeStudents();
-//        System.out.println(studentDetails);
-//       studentManagement.displayAllStudents();
-        System.out.println(studentManagement.totalNumberOfStudents());
+        FacultyManagement facultyManagement = new FacultyManagement();
+//        facultyManagement.addFaculty();
+//        facultyManagement.displayAllStudents();
+//        facultyManagement.addFaculty();
+//        facultyManagement.displayAllStudents();
+        FacultyDetails employee = new FacultyDetails(123,"john","wick");
+        facultyManagement.checkIn(employee);
+        facultyManagement.checkIn(employee);
+
+
 //
+//        StudentManagement studentManagement = new StudentManagement();
 //        studentManagement.addStudents();
-//        studentManagement.displayAllStudents();
-//        studentManagement.getstudents();
+////        StudentDetails studentDetails = studentManagement.removeStudents();
+////        System.out.println(studentDetails);
+////       studentManagement.displayAllStudents();
+//        System.out.println(studentManagement.totalNumberOfStudents());
+////
+////        studentManagement.addStudents();
+////        studentManagement.displayAllStudents();
+////        studentManagement.getstudents();
     }
 }
