a86014892f7fdad613ff099ac3ce5c35df28521b
Sohan Kotha <sohankotha@gmail.com>
2020-08-31 15:35:17 +0530

    Finished

--- src/com/commandline/facultyMangement/FacultyManagement.java
+++ src/com/commandline/facultyMangement/FacultyManagement.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class FacultyManagement {
 
     private int totalTime = 0;
+    private boolean yes;
 
     public void checkIn(FacultyDetails facultyDetails) {
         Scanner scanner = new Scanner(System.in);
@@ -60,7 +61,27 @@ public class FacultyManagement {
         facultyDetails.setOutStandingDues(dues);
         System.out.println("Sir/Madam do you want to recommend any books to the students");
         facultyDetails.recommendedBooks.add(new Book(scanner.nextInt(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextInt()));
-        scanner.close();
+
+        System.out.println("Sir/Madam you have an OutstandingDue of : Rs." + facultyDetails.getOutStandingDues() + " would you like to pay this amount?");
+        System.out.println("Press Y for yes and N for no");
+        String response = scanner.nextLine().toLowerCase();
+        if (response == "y") {
+            System.out.println("How much amount would you like to pay Sir/Madam");
+            int facultyMoney = scanner.nextInt();
+            if (facultyMoney < dues) {
+                dues = facultyMoney - dues;
+                System.out.println("Sir/Madam after paying your dues you are still left with a due of : Rs." + dues);
+                scanner.close();
+            } else if (facultyMoney == dues) {
+                dues = 0;
+                System.out.println("Sir/Madam thanks for clearing your total due");
+            } else {
+                facultyMoney = facultyMoney - dues;
+                System.out.println("Sir/Madam thanks for clearing your total due,After paying you are left with a change of : Rs." + facultyMoney);
+            }
+        } else {
+            System.out.println("Sir/Madam Thanks for visiting");
+        }
     }
 }
 
